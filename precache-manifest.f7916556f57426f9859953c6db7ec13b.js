e182d1b93f15afa3e50e2d78118fe6cad7a212cd
lpg0 <lukepg98@gmail.com>
2020-07-16 11:30:15 -0400

    Updates

---
+++ precache-manifest.f7916556f57426f9859953c6db7ec13b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf9b648f436a72441a93bac9e93de94b",
+    "url": "/website_8/index.html"
+  },
+  {
+    "revision": "7996c1e8e0285347c75b",
+    "url": "/website_8/static/css/main.1453880f.chunk.css"
+  },
+  {
+    "revision": "db3f26cf6c3b1951f749",
+    "url": "/website_8/static/js/2.eacb0eac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/website_8/static/js/2.eacb0eac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7996c1e8e0285347c75b",
+    "url": "/website_8/static/js/main.d6b1be79.chunk.js"
+  },
+  {
+    "revision": "4029a90e0ef29c4f34cf",
+    "url": "/website_8/static/js/runtime-main.17c601cd.js"
+  },
+  {
+    "revision": "ef17e56a538104ead770b39449baeed3",
+    "url": "/website_8/static/media/website_8-01.ef17e56a.svg"
+  },
+  {
+    "revision": "ca10664c3a20f1935dbea5bbc3e22aeb",
+    "url": "/website_8/static/media/website_8-02.ca10664c.svg"
+  },
+  {
+    "revision": "370a94ce322125f1a59d5f9398f0898c",
+    "url": "/website_8/static/media/website_8-03.370a94ce.svg"
+  }
+]);
